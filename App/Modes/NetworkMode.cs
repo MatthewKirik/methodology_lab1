e70ad38ccbe17bfe7b69fe85da48bf915a610ab2
Матвей Кирик <ikirik59@gmail.com>
2022-02-21 18:09:28 +0200

    Add NetworkMode class
    This will be very super great mode that will work over the internet.

---
+++ App/Modes/NetworkMode.cs
@@ -0,0 +1,11 @@
+namespace App.Modes
+{
+    public class NetworkMode : IMode
+    {
+        public override void Start()
+        {
+            //TODO: implement cool mode to solve equations via the Internet
+            throw new System.NotImplementedException();
+        }
+    }
+}
