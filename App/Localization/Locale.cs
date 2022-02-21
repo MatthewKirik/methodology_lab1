eac83bf8be0ef37c775e55716f99e1fab50e54b7
Матвей Кирик <ikirik59@gmail.com>
2022-02-21 17:14:59 +0200

    Add Locale class and fix usages
    Moved all strings to the separate class to improve readability and allow repeated usage of messages.

---
+++ App/Localization/Locale.cs
@@ -0,0 +1,28 @@
+namespace App.Localization
+{
+    public static class Locale
+    {
+        public static string GreetingMsg = "Hello! Please, enter a, b and c coefficients " +
+                                            "as in an equation ax^2+bx+c=0";
+        
+        public static string APrompt = "a = ";
+        
+        public static string BPrompt = "b = ";
+        
+        public static string CPrompt = "c = ";
+        
+        public static string ParsingFailedMsg = "Sorry, parsing failed for the string you entered. " +
+                                                 "Please, try again.";
+        
+        public static string FinalEquationMsg (double a, double b, double c) 
+            => $"Equation is: ({a})x^2 + ({b})x + ({c}) = 0";
+        
+        public static string NoRootsMsg = "There are no roots for this equation";
+        
+        public static string SingleRootMsg(double x) 
+            => $"There is single root x = {x}";
+        
+        public static string TwoRootsMsg(double x1, double x2) 
+            => $"There are two roots: x1 = {x1}; x2 = {x2}";
+    }
+}
--- App/Modes/InteractiveMode.cs
+++ App/Modes/InteractiveMode.cs
@@ -1,24 +1,10 @@
 using System;
+using App.Localization;
 
 namespace App.Modes
 {
     public class InteractiveMode : IMode
     {
-        private static string greetingMsg = "Hello! Please, enter a, b and c coefficients " +
-                                            "as in an equation ax^2+bx+c=0";
-        private static string aPrompt = "a = ";
-        private static string bPrompt = "b = ";
-        private static string cPrompt = "c = ";
-        private static string parsingFailedMsg = "Sorry, parsing failed for the string you entered. " +
-                                                 "Please, try again.";
-        private static string finalEquationMsg (double a, double b, double c) 
-            => $"Equation is: ({a})x^2 + ({b})x + ({c}) = 0";
-        private static string noRootsMsg = "There are no roots for this equation";
-        private static string singleRootMsg(double x) 
-            => $"There is single root x = {x}";
-        private static string twoRootsMsg(double x1, double x2) 
-            => $"There are two roots: x1 = {x1}; x2 = {x2}";
-        
         public void Start()
         {
             var (a, b, c) = ReadData();
@@ -35,29 +21,29 @@ namespace App.Modes
                 string input = Console.ReadLine();
                 isInputCorrect = double.TryParse(input, out result);
                 if (!isInputCorrect)
-                    Console.WriteLine(parsingFailedMsg);
+                    Console.WriteLine(Locale.ParsingFailedMsg);
             }
             return result;
         }
         
         private static (double a, double b, double c) ReadData()
         {
-            Console.WriteLine(greetingMsg);
-            var a = ReadInt(aPrompt);
-            var b = ReadInt(bPrompt);
-            var c = ReadInt(cPrompt);
+            Console.WriteLine(Locale.GreetingMsg);
+            var a = ReadInt(Locale.APrompt);
+            var b = ReadInt(Locale.BPrompt);
+            var c = ReadInt(Locale.CPrompt);
             return (a, b, c);
          }
 
         private static void Solve(double a, double b, double c)
         {
-            Console.WriteLine(finalEquationMsg(a, b, c));
+            Console.WriteLine(Locale.FinalEquationMsg(a, b, c));
             
             // find discriminant of the equation (https://en.wikipedia.org/wiki/Discriminant) 
             double d = b * b - 4 * a * c; 
             
             if (d < 0)
-                Console.WriteLine(noRootsMsg);
+                Console.WriteLine(Locale.NoRootsMsg);
             else if (d == 0)
                 SolveForSingleRoot(a, b);
             else
@@ -67,14 +53,14 @@ namespace App.Modes
         private static void SolveForSingleRoot(double a, double b)
         {
             double x = -b / (2 * a);
-            Console.WriteLine(singleRootMsg(x));
+            Console.WriteLine(Locale.SingleRootMsg(x));
         }
         private static void SolveForTwoRoots(double a, double b, double d)
         {
             double sqrtD = Math.Sqrt(d);
             double x1 = (-b + sqrtD) / (2 * a);
             double x2 = (-b - sqrtD) / (2 * a);
-            Console.WriteLine(twoRootsMsg(x1, x2));
+            Console.WriteLine(Locale.TwoRootsMsg(x1, x2));
         }
     }
 }
