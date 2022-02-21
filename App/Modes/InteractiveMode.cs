68bb0d52f1317d53500bbe89e52376e41a350b57
Матвей Кирик <ikirik59@gmail.com>
2022-02-21 16:30:36 +0200

    Hard-code english localization data.
    These lines should be moved to the localization files afterwards, but were put right into the code as there is very little chance that other languages will be required.

--- App/Modes/InteractiveMode.cs
+++ App/Modes/InteractiveMode.cs
@@ -2,6 +2,14 @@ namespace App.Modes
 {
     public class InteractiveMode : IMode
     {
+        private static string greetingMsg = "Hello! Please, enter a, b and c coefficients " +
+                                         "as in an equation ax^2+bx+c=0";
+        private static string aPrompt = "a = ";
+        private static string bPrompt = "b = ";
+        private static string cPrompt = "c = ";
+        private static string parsingFailedMsg = "Sorry, parsing failed for the string you entered. " +
+                                                 "Please, try again.";
+        
         public void Start()
         {
             // TODO: Implement solving equations in interactive mode.
