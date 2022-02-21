c01bdeade864be31e3e4d6ca135ea4f179dfa070
Матвей Кирик <ikirik59@gmail.com>
2022-02-21 17:51:38 +0200

    Add ReadData method
    This method is used to read coefficients from the file specified on mode instantiation.

--- App/Modes/FileMode.cs
+++ App/Modes/FileMode.cs
@@ -1,11 +1,43 @@
+using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using App.Localization;
+using App.Math;
+
 namespace App.Modes
 {
     public class FileMode : IMode
     {
+        private string filePath;
+        public FileMode(string filePath)
+        {
+            this.filePath = filePath;
+        }
+        
         public void Start()
         {
             // TODO: Implement file mode to solve equations using file input
             throw new System.NotImplementedException();
+        }
+        
+        private static (double a, double b, double c) ReadData(string path)
+        {
+            string data = File.ReadAllText(path);
+            var parts = data.Split();
+            if (parts.Length > 0)
+                PrintErrorAndExit(Locale.ParsingFailedMsg);
+            bool aParsed = double.TryParse(parts[0], out double a);
+            bool bParsed = double.TryParse(parts[1], out double b);
+            bool cParsed = double.TryParse(parts[2], out double c);
+            if (!aParsed || !bParsed || !cParsed)
+                PrintErrorAndExit(Locale.ParsingFailedMsg);
+            return (a, b, c);
+        }
+
+        private static void PrintErrorAndExit(string error)
+        {
+            Console.WriteLine(error);
+            Environment.Exit(1);
         }
     }
 }
