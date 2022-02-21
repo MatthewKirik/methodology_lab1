e2bc23b50c5d21a2a8420ae34d7eee0930d5e391
Матвей Кирик <ikirik59@gmail.com>
2022-02-21 17:32:09 +0200

    Add FileMode class
    This class will implement IMode interface and will be used to solve equations using files (coefficients input).

---
+++ App/Modes/FileMode.cs
@@ -0,0 +1,11 @@
+namespace App.Modes
+{
+    public class FileMode : IMode
+    {
+        public void Start()
+        {
+            // TODO: Implement file mode to solve equations using file input
+            throw new System.NotImplementedException();
+        }
+    }
+}
