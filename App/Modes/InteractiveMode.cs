d785d26232275760c22ce62a65330ea516f9d1ac
Матвей Кирик <ikirik59@gmail.com>
2022-02-21 16:54:48 +0200

    Add Solve function
    This function takes care of the actual equation solving and result output.
    Functions for the partial cases were added as well and are still to be implemented.

--- App/Modes/InteractiveMode.cs
+++ App/Modes/InteractiveMode.cs
@@ -5,12 +5,15 @@ namespace App.Modes
     public class InteractiveMode : IMode
     {
         private static string greetingMsg = "Hello! Please, enter a, b and c coefficients " +
-                                         "as in an equation ax^2+bx+c=0";
+                                            "as in an equation ax^2+bx+c=0";
         private static string aPrompt = "a = ";
         private static string bPrompt = "b = ";
         private static string cPrompt = "c = ";
         private static string parsingFailedMsg = "Sorry, parsing failed for the string you entered. " +
                                                  "Please, try again.";
+        private static string finalEquationMsg (double a, double b, double c) 
+            => $"Equation is: ({a})x^2 + ({b})x + ({c}) = 0";
+        private static string noRootsMsg = "There are no roots for this equation";
         
         public void Start()
         {
@@ -39,6 +42,32 @@ namespace App.Modes
             var b = ReadInt(bPrompt);
             var c = ReadInt(cPrompt);
             return (a, b, c);
+        }
+
+        private static void Solve(double a, double b, double c)
+        {
+            Console.WriteLine(finalEquationMsg(a, b, c));
+            
+            // find discriminant of the equation (https://en.wikipedia.org/wiki/Discriminant) 
+            double d = b * b - 4 * a * c; 
+            
+            if (d < 0)
+                Console.WriteLine(noRootsMsg);
+            else if (d == 0)
+                SolveForSingleRoot(a, b, c);
+            else
+                SolveForTwoRoots(a, b, c);
+        }
+
+        private static void SolveForSingleRoot(double a, double b, double c)
+        {
+            // TODO: Implement solving an equation for single root (d=0)
+            throw new NotImplementedException();
+        }
+        private static void SolveForTwoRoots(double a, double b, double c)
+        {
+            // TODO: Implement solving an equation for two roots (d>0)
+            throw new NotImplementedException();
         }
     }
 }
